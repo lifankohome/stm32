ce8c9e5184570329cb519c9b44c91fee3db824da
lifanko <lifankohome@163.com>
2022-03-05 12:27:33 +0800

    IR is OK

--- F401/IR/Core/Src/main.c
+++ F401/IR/Core/Src/main.c
@@ -93,7 +93,7 @@ int main(void)
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   
-  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
   
   /* USER CODE END 2 */
 
